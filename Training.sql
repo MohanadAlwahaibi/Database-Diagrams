29c7465acbc8759b952a02f5dfb2d1c7e19b05cb
MuhannadAlwahaibi <alwahaibi613@gmail.com>
2025-06-19 14:13:04 +0400

    done with the Training sql

--- Training.sql
+++ Training.sql
@@ -65,5 +65,76 @@ select * from project;
 select * from Dependent;
 
 ALTER TABLE Employee
-add Dnum int foreign key (Dnum) REFERENCES Department (Dnum) not null;
+add Dnum int foreign key (Dnum) REFERENCES Department (Dnum)
 
+ALTER TABLE Departement
+DROP CONSTRAINT FK__Departement__SSN__2D27B809
+
+alter table Employee
+DROP CONSTRAINT FK__Employee__Superv__29572725
+
+insert into Departement(Dname,Hiredate,Dnum,SSN)
+values                 ('IT','1983-08-22',1,1)
+
+insert into Departement(Dname,Hiredate,Dnum,SSN)
+values                 ('SC','1983-04-12',2,2)
+
+insert into Employee(Fname,Lname,Gender,Supervisor,Birthdate,Dnum)
+values              ('Muhannad','alwahaibi',1,100,'1938-12-31',1)
+
+insert into Employee(Fname,Lname,Gender,Supervisor,Birthdate,Dnum)
+values              ('Ali','alnaabi',0,200,'1938-03-11',2)
+
+select * from Employee
+select * from Departement
+select * from Dependent
+
+insert into Dependent (Name,Birthdate,Gender,SSN)
+values                ('Muhannad','1928-03-23',1,1)
+
+insert into Dependent (Name,Birthdate,Gender,SSN)
+values                ('Ali','1928-06-22',1,2)
+
+alter table Dependent
+DROP CONSTRAINT FK__Dependent__SSN__3B75D760
+
+select * from Dependent
+
+select * from Department_location
+
+insert into Department_location(Dnum,Location)
+values                (1,'Muscat')
+
+insert into Department_location (Dnum,Location)
+values                           (2,'Nizwa')
+
+alter table Department_location
+DROP CONSTRAINT FK__Department__Dnum__37A5467C
+
+SELECT * FROM Project
+
+insert into project (City,Pnumber,Dnum,Location,PName)
+values               ('Muscat',1,1,'Oman','TECH')
+
+insert into project (City,Pnumber,Dnum,Location,PName)
+values               ('Nizwa',2,2,'Oman','ITSEC')
+
+alter table project
+DROP CONSTRAINT FK__project__Dnum__3E52440B
+
+select * from Works_in
+
+insert into Works_in (Workinghour,SSN,Pnumber)
+values               ('05:24:23',1,1)
+
+insert into Works_in (Workinghour,SSN,Pnumber)
+values               ('04:27:25',2,2)
+
+UPDATE Works_in
+SET Pnumber=3
+WHERE SSN=2
+
+DELETE FROM Employee
+WHERE Fname = 'Muhannad' AND SSN=17
+
+SELECT * FROM Employee
